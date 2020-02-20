ec57a9112eec8dec32635ec4e6a7a0d89570f205
umakanth pendyala <umakanthpendyala@gmail.com>
2020-02-20 17:51:09 +0530

    this is image processing with python OpenCV and tesseract

---
+++ test.py
@@ -0,0 +1,8 @@
+import cv2
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+img = cv2.imread("Mydownload.jpg")
+text = ""
+text = pytesseract.image_to_string(img)
+print(str(text))
